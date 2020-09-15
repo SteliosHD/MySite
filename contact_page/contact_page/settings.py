2beb6f5c110776f74e56b06a1dde97afb43b2533
Stelios Bitzas <hodropetsos@Ubuntu19.10>
2020-09-15 11:02:09 +0300

    before cleaning code

--- contact_page/contact_page/settings.py
+++ contact_page/contact_page/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = config['SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['172.105.65.236']
+ALLOWED_HOSTS = ['172.105.65.236', 'localhost']
 
 
 # Application definition
